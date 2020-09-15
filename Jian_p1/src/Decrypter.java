176d3c4b4bcc95902ab6a8649a229c0ee5a252b9
Xinyi <xyj8898@live.com>
2020-09-14 23:50:29 -0400

    Wrote helper encryptDigits and reverseModulusOperator functions

--- Jian_p1/src/Decrypter.java
+++ Jian_p1/src/Decrypter.java
@@ -1,2 +1,53 @@
-package PACKAGE_NAME;public class Decrypter {
+public class Decrypter
+{
+    public String decrypt(String input)
+    {
+        int i, val;
+        char temp;
+        String decrypted  = "", ans = "";
+
+        decrypted += input.charAt(2);
+        decrypted += input.charAt(3);
+        decrypted += input.charAt(0);
+        decrypted += input.charAt(1);
+
+        for (i = 0; i < 4; i++)
+        {
+            val = reverseModulusOperator(decrypted.charAt(i));
+            if (val > 0)
+            {
+                val -= 7;
+                ans += val;
+            }
+            else
+                System.out.println("An error occurred");
+        }
+
+        return ans;
+    }
+
+    public int reverseModulusOperator(int value)
+    {
+        if (value == 0)
+            return 10;
+        if (value == 1)
+            return 11;
+        if (value == 2)
+            return 12;
+        if (value == 3)
+            return 13;
+        if (value == 4)
+            return 14;
+        if (value == 5)
+            return 15;
+        if (value == 6)
+            return 16;
+        if (value == 7)
+            return 7;
+        if (value == 8)
+            return 8;
+        if (value == 9)
+            return 9;
+        return -1;
+    }
 }
--- Jian_p1/src/Encrypter.java
+++ Jian_p1/src/Encrypter.java
@@ -1,4 +1,31 @@
 public class Encrypter
 {
-    
+    public String encrypt(String input)
+    {
+        int i;
+        char temp;
+        String encrypted  = "", ans = "";
+
+        for (i = 0; i < 4; i++)
+        {
+            encrypted += encryptDigits(input.charAt(i));
+        }
+
+        ans += encrypted.charAt(2);
+        ans += encrypted.charAt(3);
+        ans += encrypted.charAt(0);
+        ans += encrypted.charAt(1);
+
+        return ans;
+    }
+
+    public int encryptDigits (int value)
+    {
+        int i;
+
+        value = ((value + 7) % 10);
+
+        return value;
+    }
+
 }

7f75581591937dd2cc4c9a1d7f0f0524f701bad4
Xinyi <xyj8898@live.com>
2020-09-29 12:51:48 -0400

    Fixed the test cases so that they can run

--- Jian_p2/src/BodyMassIndexTest.java
+++ Jian_p2/src/BodyMassIndexTest.java
@@ -1,28 +1,66 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BodyMassIndexTest {
-    @Test
-    public void testCalculateBodyMassIndex() {
-        BodyMassIndex c = new BodyMassIndex(62.0, 104.8);
-        assertEquals(19.1, c.calculateBodyMassIndex(62.0, 104.8));
+    private BodyMassIndex bodyMassIndex;
+
+    @BeforeEach
+    public void init(){
+        bodyMassIndex = new BodyMassIndex(62.0, 102.8);
     }
 
     @Test
-    public void testBodyMassIndexBMICategory() {
-        double bmi = 18.1;
-        BodyMassIndex c = new BodyMassIndex(62.0, 104.8);
-        assertEquals("Underweight", c.bmiCategory(bmi));
-        bmi = 19.1;
-        assertEquals("Normal weight", c.bmiCategory(bmi));
-        bmi = 24.9;
-        assertEquals("Normal weight", c.bmiCategory(bmi));
-        bmi = 25;
-        assertEquals("Overweight", c.bmiCategory(bmi));
-        bmi = 29.9;
-        assertEquals("Overweight", c.bmiCategory(bmi));
-        bmi = 32;
-        assertEquals("Obese", c.bmiCategory(bmi));
+    public void testCalculateBodyMassIndex() {
+        double bmi = bodyMassIndex.calculateBodyMassIndex(62.0, 102.8);
+        BigDecimal bmiToTheTenthsPlace = new BigDecimal(bmi).setScale(1, RoundingMode.HALF_EVEN);
+        bmi = bmiToTheTenthsPlace.doubleValue();
+        assertEquals(18.8, bmi);
+    }
+
+    @Test
+    public void testBodyMassIndexBMICategoryUnderweight() {
+        double height = 62.0;
+        double weight = 98.2;
+        assertEquals("Underweight", bodyMassIndex.bmiCategory(height, weight));
+    }
+
+    @Test
+    public void testBodyMassIndexBMICategoryNormalWeightLowerBound() {
+        double height = 62.5;
+        double weight = 102.8;
+        assertEquals("Normal weight", bodyMassIndex.bmiCategory(height, weight));
+    }
+
+    @Test
+    public void testBodyMassIndexBMICategoryNormalWeightUpperBound() {
+        double height = 63.3;
+        double weight = 142;
+        assertEquals("Normal weight", bodyMassIndex.bmiCategory(height, weight));
+    }
+
+    @Test
+    public void testBodyMassIndexBMICategoryOverweightLowerBound() {
+        double height = 77.2;
+        double weight = 212;
+        assertEquals("Overweight", bodyMassIndex.bmiCategory(height, weight));
+    }
+
+    @Test
+    public void testBodyMassIndexBMICategoryOverweightUpperBound() {
+        double height = 49.6;
+        double weight = 104.8;
+        assertEquals("Overweight", bodyMassIndex.bmiCategory(height, weight));
+    }
+
+    @Test
+    public void testBodyMassIndexBMICategoryObese() {
+        double height = 52.8;
+        double weight = 119;
+        assertEquals("Obese", bodyMassIndex.bmiCategory(height, weight));
     }
 }

ea4e6c8b68792140600f614d895b410bfda48924
Xinyi <xyj8898@live.com>
2020-10-12 16:52:21 -0400

    Fixed spacing in Shape2D and Shape3D abstract classes

--- Jian_p3/src/Shape2D.java
+++ Jian_p3/src/Shape2D.java
@@ -1,5 +1,6 @@
 public abstract class Shape2D extends Shape {
     Shape2D(){
     }
+
     public abstract double getArea();
 }
--- Jian_p3/src/Shape3D.java
+++ Jian_p3/src/Shape3D.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class Shape3D {
+public abstract class Shape3D extends Shape2D {
+    Shape3D(){
+    }
+
+    public abstract double getVolume();
 }

8b248ee5b01e9dd39440369f8786e9a0b2053faa
Xinyi <xyj8898@live.com>
2020-10-12 16:40:55 -0400

    Created Pyramid class and its methods

---
+++ Jian_p3/src/Pyramid.java
@@ -0,0 +1,31 @@
+public class Pyramid extends Shape3D{
+    private double length, width, height;
+    private String name = "pyramid";
+    private double surfaceArea, term1, radicand1, radicand2, volume;
+
+    public Pyramid(double length, double width, double height){
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public String getName(){
+        return name;
+    }
+
+    @Override
+    public double getArea(){
+        // A = lw + l*sqrt((w/2)^2 + h^2) + w
+        radicand1 = Math.pow((width/2.0), 2.0) + Math.pow(height, 2.0);
+        radicand2 = Math.pow((length/2.0), 2.0) + Math.pow(height, 2.0);
+        surfaceArea = (length * width) + (length * Math.sqrt(radicand1)) + (width * Math.sqrt(radicand2));
+        return surfaceArea;
+    }
+
+    @Override
+    public double getVolume(){
+        volume = (1.0/3.0) * length * width * height;
+        return volume;
+    }
+}
